c07232c442b5ff5efeb8cf28996536c1e02771bf
Brendan Denzel <77647191+BrendanDenzel@users.noreply.github.com>
2025-04-29 16:58:21 -0400

    Update fade_in.js

--- dist/fade_in.js
+++ dist/fade_in.js
@@ -1,26 +1,4 @@
-window.onload = function () {
-  const body = document.querySelector("body");
-
-  // Make the body visible after a short delay (optional, adjust as needed)
-  setTimeout(() => {
-    body.classList.remove("opacity-0");
-    body.classList.add("opacity-100");
-
-    // Then start fading in images
-    const photos = document.querySelectorAll("img");
-    let delay = 0;
-    photos.forEach((photo) => {
-      setTimeout(() => {
-        photo.classList.remove("opacity-0");
-        photo.classList.add("opacity-100");
-      }, delay);
-      delay += 100;
-    });
-  }, 2000); // delay after full load before starting fade-ins
-};
-
-
-/* document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   body.classList.remove("opacity-0");
   body.classList.add("opacity-100");
@@ -33,4 +11,4 @@ window.onload = function () {
     }, delay);
     delay += 100;
   });
-}); */
+}); 
